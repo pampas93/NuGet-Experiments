5eda1a8bc20cb2397d3b3272933457e3bb7bb316
pampas93 <abhijit.93@hotmail.com>
2020-04-02 22:59:27 -0600

    scene setup with dll import functions

---
+++ NuGetInUnity/Assets/Scripts/TestScript.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    [DllImport("SimpleLib", EntryPoint = "AddNums")]
+    public static extern float AddNumbers(float a, float b);
+
+    [DllImport("SimpleLib", EntryPoint = "SubtractNums")]
+    public static extern float SubtractNumbers(float a, float b);
+
+    [DllImport("SimpleLib", EntryPoint = "MultiplyNums")]
+    public static extern float MultiplyNumbers(float a, float b);
+
+    [SerializeField]
+    private Text result;
+
+    void Start()
+    {
+        try 
+        {
+            float r = MultiplyNumbers(45, 67.7f);
+            result.text = "45 * 67.7 = " + r;
+        }
+        catch (Exception ex)
+        {
+            result.text = "DllNotFoundException: SimpleLib";
+        }
+    }
+}
